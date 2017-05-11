da8b9fb139b93c4a4fecb8abdab71dfab8a3fdec
Kari Newhouse <newhousemusic@gmail.com>
2017-05-11 16:08:05 -0700

    Made navigation and view controller for tasks

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Kari Newhouse on 5/11/17.
+//  Copyright © 2017 Kari Code. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
